31bf7868faf168ab1cf696ac59ede07d477bbab6
mahendiran-ship-it <mahentiran341995@gmail.com>
2026-05-11 23:16:30 +0530

    Update app.py
    I Update a Small Logic in app.py

--- app.py
+++ app.py
@@ -7,13 +7,18 @@ import sklearn
 st.title("Local Marketplace Price Predictor")
 st.write("Created By Mahendiran")
 
-age = st.slider("Phone Age (Years)", 0, 5, 1)
+age = st.slider("Phone Age (Years)", 0, 7, 1)
 price = st.number_input("Original Price", value=10000)
 
 if st.button("Calculate Prediction"):
     model = joblib.load('phone_model.pkl')
-    prediction = model.predict([[age, price]])
     
+    
+        
+    raw_prediction = model.predict([[age, price]])[0]
+    final_price = max(0, raw_prediction)
+    
+    st.success(f"Predicted Selling Price: ₹{final_price:,.2f}")
     
          
     st.success(f"Predicted Selling Price: ₹{prediction[0]:,.2f}")
