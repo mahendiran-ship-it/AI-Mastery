333a107bcb13c168efc967e876bab3128624f1f0
Mahendiran <mahentiran341995@gmail.com>
2026-05-11 22:59:05 +0530

    Update app logic

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 #we use streamlit to build tge interface twin
 
 import streamlit as st
-import joblib  <-- Add a 
-import sklearn <-- Add a 
+import joblib  
+import sklearn 
 
 st.title("Local Marketplace Price Predictor")
 
@@ -11,8 +11,8 @@ age = st.slider("Phone Age (Years)", 0, 5, 1)
 price = st.number_input("Original Price", value=10000)
 
 if st.button("Calculate Prediction"):
-    model = joblib.load('phone_model.pkl') <-- Add a 
-    prediction = model.predict([[age, price]]) <-- Add a 
+    model = joblib.load('phone_model.pkl')
+    prediction = model.predict([[age, price]])
     
     
          
